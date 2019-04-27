673000bbece5f34192af10688b695a1ce89a1bba
unknown <bagus.dharma@gmail.com>
2019-04-27 09:15:15 +0700

    fix minor bug migrations

--- database/migrations/2019_04_22_155602_create_users.php
+++ database/migrations/2019_04_22_155602_create_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTeacher extends Migration
+class CreateUsers extends Migration
 {
     /**
      * Run the migrations.
